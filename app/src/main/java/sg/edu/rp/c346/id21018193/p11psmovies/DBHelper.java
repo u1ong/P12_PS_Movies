c82bf05e834808c1ad1aba7607b73eea4f8b1fa6
ButtorBo <21044912@myrp.edu.sg>
2022-07-27 12:22:10 +0800

    DBHelper create n upgrade

--- app/src/main/java/sg/edu/rp/c346/id21018193/p11psmovies/DBHelper.java
+++ app/src/main/java/sg/edu/rp/c346/id21018193/p11psmovies/DBHelper.java
@@ -1,7 +1,9 @@
 package sg.edu.rp.c346.id21018193.p11psmovies;
 
 import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
 
 public class DBHelper extends SQLiteOpenHelper {
 
@@ -15,6 +17,25 @@ public class DBHelper extends SQLiteOpenHelper {
     private static final String COLUMN_RATING = "rating";
     public DBHelper(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        String createNoteTableSql = "CREATE TABLE " + TABLE_MOVIE + "("
+                + COLUMN_ID + " INTEGER PRIMARY KEY AUTOINCREMENT,"
+                + COLUMN_TITLE + " TEXT, "
+                + COLUMN_GENRE + " TEXT, "
+                + COLUMN_YEAR + " INTEGER, "
+                + COLUMN_RATING +" TEXT ) ";
+        db.execSQL(createNoteTableSql);
+
+        Log.i("info", "created tables");
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int i, int i1) {
+        db.execSQL("ALTER TABLE " + TABLE_MOVIE + " ADD COLUMN  module_name TEXT ");
+        onCreate(db);
     }
 
 }
