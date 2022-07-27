8d4b0b54872d250c21ac0cd6131561ca377c849a
lee zhuo hong <21018193@myrp.edu.sg>
2022-07-27 13:37:18 +0800

    Zhuo hong - commit

--- app/src/main/java/sg/edu/rp/c346/id21018193/p11psmovies/MainActivity.java
+++ app/src/main/java/sg/edu/rp/c346/id21018193/p11psmovies/MainActivity.java
@@ -2,6 +2,7 @@ package sg.edu.rp.c346.id21018193.p11psmovies;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -10,7 +11,7 @@ import android.widget.ListView;
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
-    Button btnShow;
+    Button btnShow, btnInsert;
     ListView lvMovies;
     ArrayList<Movies> alMoviesList;
     CustomAdapter caMovies;
@@ -21,12 +22,26 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         //test
         btnShow = findViewById(R.id.buttonShow);
+        btnInsert = findViewById(R.id.buttonInsert);
         btnShow.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                DBHelper dbh = new DBHelper(MainActivity.this);
+                alMoviesList.clear();
+                alMoviesList.addAll(dbh.getAllMovie());
+                caMovies.notifyDataSetChanged();
             }
         });
+
+        btnInsert.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(MainActivity.this,
+                       InsertActivity.class);
+                startActivity(i);
+            }
+        });
+
         lvMovies = findViewById(R.id.listViewMovies);
         alMoviesList = new ArrayList<>();
 
