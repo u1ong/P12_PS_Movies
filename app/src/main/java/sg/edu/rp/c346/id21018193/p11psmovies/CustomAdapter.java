d70622e3511e33db197bd421a18b314ca41e7ac6
lee zhuo hong <21018193@myrp.edu.sg>
2022-07-27 11:50:17 +0800

    Zhuo hong - commit

---
+++ app/src/main/java/sg/edu/rp/c346/id21018193/p11psmovies/CustomAdapter.java
@@ -0,0 +1,52 @@
+package sg.edu.rp.c346.id21018193.p11psmovies;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+
+public class CustomAdapter extends ArrayAdapter {
+    Context parent_context;
+    int layout_id;
+    ArrayList<Movies> movieList;
+
+    public CustomAdapter(Context context, int resource, ArrayList<Movies> objects) {
+        super(context, resource, objects);
+        parent_context = context;
+        layout_id = resource;
+        movieList = objects;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        LayoutInflater inflater = (LayoutInflater)
+                parent_context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
+        View rowView = inflater.inflate(layout_id, parent, false);
+
+        TextView tvName = rowView.findViewById(R.id.textViewName);
+        TextView tvCode = rowView.findViewById(R.id.textViewCountryCode);
+        TextView tvNum = rowView.findViewById(R.id.textViewPhoneNum);
+        ImageView ivGender = rowView.findViewById(R.id.imageViewGender);
+
+        Contact currentItem = contactList.get(position);
+
+        tvName.setText(currentItem.getName());
+        tvCode.setText("+" + currentItem.getCountryCode());
+        tvNum.setText(currentItem.getPhoneNum() + "");
+        if (currentItem.getGender() == 'F') {
+            ivGender.setImageResource(R.drawable.female);
+        }
+        else {
+            ivGender.setImageResource(R.drawable.male);
+        }
+
+        return rowView;
+    }
+}
