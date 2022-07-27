2ffa0125738e55dfcab6453fd538014f3f61ff5b
lee zhuo hong <21018193@myrp.edu.sg>
2022-07-27 11:43:27 +0800

    Zhuo hong - commit

---
+++ app/src/main/java/sg/edu/rp/c346/id21018193/p11psmovies/Movies.java
@@ -0,0 +1,56 @@
+package sg.edu.rp.c346.id21018193.p11psmovies;
+
+public class Movies {
+    private String title;
+    private String genre;
+    private int year;
+    private String rating;
+
+    public Movies(String title, String genre, int year, String rating) {
+        this.title = title;
+        this.genre = genre;
+        this.year = year;
+        this.rating = rating;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public String getRating() {
+        return rating;
+    }
+
+    public void setRating(String rating) {
+        this.rating = rating;
+    }
+
+    public String toString() {
+        return "Movie {" +
+                "Title ='" + title + '\'' +
+                ", genre ='" + genre + '\'' +
+                ", year ='" + year + '\'' +
+                ", rating ='" + rating + '\'' +
+                '}';
+    }
+}
