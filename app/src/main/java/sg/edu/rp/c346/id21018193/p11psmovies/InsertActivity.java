5a32d08aee588aefdc77ab0f9661689c184e8929
ButtorBo <21044912@myrp.edu.sg>
2022-07-27 12:11:55 +0800

    Coding insert and showlist

--- app/src/main/java/sg/edu/rp/c346/id21018193/p11psmovies/InsertActivity.java
+++ app/src/main/java/sg/edu/rp/c346/id21018193/p11psmovies/InsertActivity.java
@@ -3,12 +3,35 @@ package sg.edu.rp.c346.id21018193.p11psmovies;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
 
 public class InsertActivity extends AppCompatActivity {
+
+    EditText title, genre, year;
+    Button btnInsert, btnShowList;
+    Spinner rating;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_insert);
+
+        title = findViewById(R.id.etTitle);
+        genre = findViewById(R.id.etGenre);
+        year = findViewById(R.id.etYear);
+        btnInsert = findViewById(R.id.btnAdd);
+        btnShowList = findViewById(R.id.btnReturn);
+        rating = findViewById(R.id.spinnerRates);
+
+        btnInsert.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
     }
 }
