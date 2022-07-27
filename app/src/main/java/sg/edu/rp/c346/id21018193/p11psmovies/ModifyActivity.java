d8a7f12962c741e1aff905b8fb9a0658df2a5b7b
lee zhuo hong <21018193@myrp.edu.sg>
2022-07-27 12:36:52 +0800

    Zhuo hong - commit

---
+++ app/src/main/java/sg/edu/rp/c346/id21018193/p11psmovies/ModifyActivity.java
@@ -0,0 +1,14 @@
+package sg.edu.rp.c346.id21018193.p11psmovies;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ModifyActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_modify);
+    }
+}
