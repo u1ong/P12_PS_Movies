71ebdfc2ce8fb3a120b20a068a31ceedf35c08a7
lee zhuo hong <21018193@myrp.edu.sg>
2022-07-27 13:54:54 +0800

    Zhuo hong - commit

--- app/src/main/java/sg/edu/rp/c346/id21018193/p11psmovies/ModifyActivity.java
+++ app/src/main/java/sg/edu/rp/c346/id21018193/p11psmovies/ModifyActivity.java
@@ -2,6 +2,7 @@ package sg.edu.rp.c346.id21018193.p11psmovies;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -44,7 +45,9 @@ public class ModifyActivity extends AppCompatActivity {
         btnCancel.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                Intent j = new Intent(ModifyActivity.this,
+                        MainActivity.class);
+                startActivity(j);
             }
         });
 
