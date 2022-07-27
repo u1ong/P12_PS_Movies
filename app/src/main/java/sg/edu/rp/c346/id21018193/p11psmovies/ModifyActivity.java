25ce58704fdf41f485e42cd321575287601384fc
lee zhuo hong <21018193@myrp.edu.sg>
2022-07-27 13:23:13 +0800

    Zhuo hong - commit

--- app/src/main/java/sg/edu/rp/c346/id21018193/p11psmovies/ModifyActivity.java
+++ app/src/main/java/sg/edu/rp/c346/id21018193/p11psmovies/ModifyActivity.java
@@ -3,12 +3,50 @@ package sg.edu.rp.c346.id21018193.p11psmovies;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
 
 public class ModifyActivity extends AppCompatActivity {
+    EditText title, genre, year, id;
+    Button btnUpdate, btnDelete, btnCancel;
+    Spinner rating;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_modify);
+
+        id = findViewById(R.id.etID);
+        title = findViewById(R.id.etTitle);
+        genre = findViewById(R.id.etGenre);
+        year = findViewById(R.id.etYear);
+        btnUpdate = findViewById(R.id.btnUpdate);
+        btnDelete = findViewById(R.id.btnDelete);
+        btnCancel = findViewById(R.id.buttonCancel);
+        rating = findViewById(R.id.spinnerRates);
+
+        btnUpdate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        btnDelete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        btnCancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
     }
 }
